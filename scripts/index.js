84dc344e84344613a37834f29808c8ad43ee26e0
diana-msft <diana-msft@outlook.com>
2023-03-22 02:57:35 +0300

    feat(popup): добавить события по клику - открыть закрыть

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,40 @@
 console.log("Hello World!");
 // Находим форму в DOM
-let formElement = document.querySelector('.popup');// Воспользуйтесь методом querySelector()
+// Воспользуйтесь методом querySelector()
+//делаем выборку DOM элементов
+let formElement = document.querySelector('.popup');
 let formCloseButtonElement = formElement.querySelector('.popup__close');
+let formOpenButtonElement = document.querySelector('.popup_is-opened');
+
+const openPopup = function () {
+    formElement.classList.add('popup_is-opened');
+    console.log('Форма открыта');
+};
+
+const closePopup = function () {
+    formElement.classList.remove('popup_is-opened');
+    console.log('Форма закрыта');
+};
+
+const closePopupByClickOnOverlay = function (evt) {
+    console.log(evt.target, evt.currentTarget);
+    if (evt.target !== evt.currentTarget) {
+        return;
+    }
+        closePopup();
+    };
+
+//регистрируем обработчики событий по клику
+formOpenButtonElement.addEventListener('click', openPopup);
+// formOpenButtonElement.addEventListener('click', function (event) {
+//     console.log(event);
+//  });
+formCloseButtonElement.addEventListener('click', closePopup);
+formElement.addEventListener('click', closePopupByClickOnOverlay);
+
+// togglePopupVisibility();
+
+
 // Находим поля формы в DOM
 let nameInput = document.querySelector('.popup__name');// Воспользуйтесь инструментом .querySelector()
 let jobInput = document.querySelector('.popup__job');// Воспользуйтесь инструментом .querySelector()
